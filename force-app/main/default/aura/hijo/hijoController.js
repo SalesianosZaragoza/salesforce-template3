d0bbce71eb4ceb976f4454c80857afbb85c4d70d
Gorka Sanz <gorka.sanz@salesianos.edu>
2023-02-07 09:00:39 +0100

    bugfixing

--- force-app/main/default/aura/hijo/hijoController.js
+++ force-app/main/default/aura/hijo/hijoController.js
@@ -1,7 +1,9 @@
 ({
      changeMessage : function(component, event, helper) {
-        var params = event.getParams();
+        var params = event.getParam("arguments");
+        console.log(params)
         if(params){
+            console.log(params.message)
             component.set("v.message", params.message);
         }
     }
